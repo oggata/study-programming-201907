7ce41c8aacc2044c421ad4babd86df25f1dbe8cd
Fumitoshi Ogata <ogata.fumitoshi@o-11588-mac.local>
2019-07-29 13:34:53 +0900

    update

--- src/app/bootScene.js
+++ src/app/bootScene.js
@@ -15,7 +15,6 @@ export default {
         this.load.image("bone3", require("../assets/sprites/bone/bone3.png"));
         this.load.image("bone4", require("../assets/sprites/bone/bone4.png"));
         this.load.image("bone5", require("../assets/sprites/bone/bone5.png"));
-
         this.load.audio("music", require("../assets/music/bgm_maoudamashii_ethnic11.mp3"));
         this.load.audio("music2", require("../assets/music/bgm_maoudamashii_neorock72.mp3"));
         this.load.audio("music_end", require("../assets/music/bgm_maoudamashii_acoustic49.mp3"));
@@ -24,17 +23,14 @@ export default {
         this.load.audio("se_coin", require("../assets/music/se_maoudamashii_se_sound13.mp3"));
         this.load.audio("se_destroy", require("../assets/music/se_maoudamashii_retro12.mp3"));
         this.load.audio("se_damage", require("../assets/music/se_maoudamashii_se_sound14.mp3"));
-
         this.load.spritesheet("item", require("../assets/sprites/pipo-gwspinitem009.png"), {
             frameWidth: 192,
             frameHeight: 192
         });
-
         this.load.spritesheet("meat", require("../assets/sprites/meat.png"), {
             frameWidth: 240,
             frameHeight: 240
         });
-
         this.load.spritesheet("coin", require("../assets/sprites/pipo-gwspinitem002.png"), {
             frameWidth: 192,
             frameHeight: 192
@@ -55,8 +51,6 @@ export default {
             frameWidth: 340,
             frameHeight: 340
         });
-
-
         this.load.spritesheet("enemy1", require("../assets/sprites/dinosaur/enemy1.png"), {
             frameWidth: 340,
             frameHeight: 340
@@ -69,13 +63,10 @@ export default {
             frameWidth: 340,
             frameHeight: 340
         });
-
         this.load.spritesheet("enemybig1", require("../assets/sprites/dinosaur/enemy4.png"), {
             frameWidth: 680,
             frameHeight: 680
         });
-
-
         this.load.spritesheet("doux", require("../assets/sprites/dinosaur/player.png"), {
             frameWidth: 340,
             frameHeight: 340
@@ -87,6 +78,123 @@ export default {
         });
     },
     create: function () {
+        this.anims.create({
+            key: "death",
+            frames: this.anims.generateFrameNumbers("doux", {
+                start: 3,
+                end: 3
+            }),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.anims.create({
+            key: "enemy_run",
+            frames: this.anims.generateFrameNumbers("enemy", {
+                start: 0,
+                end: 2
+            }),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.anims.create({
+            key: "enemy1_run",
+            frames: this.anims.generateFrameNumbers("enemy1", {
+                start: 0,
+                end: 2
+            }),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.anims.create({
+            key: "enemy2_run",
+            frames: this.anims.generateFrameNumbers("enemy2", {
+                start: 0,
+                end: 2
+            }),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.anims.create({
+            key: "enemy3_run",
+            frames: this.anims.generateFrameNumbers("enemy3", {
+                start: 0,
+                end: 2
+            }),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.anims.create({
+            key: "enemybig1_run",
+            frames: this.anims.generateFrameNumbers("enemybig1", {
+                start: 0,
+                end: 2
+            }),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.anims.create({
+            key: "boom",
+            frames: this.anims.generateFrameNumbers("bomb", {
+                start: 0,
+                end: 3
+            }),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.anims.create({
+            key: "fire",
+            frames: this.anims.generateFrameNumbers("fire", {
+                start: 0,
+                end: 10
+            }),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.anims.create({
+            key: "enemy_fire",
+            frames: this.anims.generateFrameNumbers("enemy_fire", {
+                start: 0,
+                end: 6
+            }),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.anims.create({
+            key: "coin",
+            frames: this.anims.generateFrameNumbers("coin", {
+                start: 0,
+                end: 6
+            }),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.anims.create({
+            key: "meat",
+            frames: this.anims.generateFrameNumbers("meat", {
+                start: 0,
+                end: 0
+            }),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.anims.create({
+            key: "item",
+            frames: this.anims.generateFrameNumbers("item", {
+                start: 0,
+                end: 6
+            }),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.anims.create({
+            key: "damaged-effect",
+            frames: this.anims.generateFrameNumbers("damaged-effect", {
+                start: 0,
+                end: 5
+            }),
+            frameRate: 10,
+            repeat: 1
+        });
         //  Our player animations, turning, walking left and walking right.
         this.anims.create({
             key: "left",
